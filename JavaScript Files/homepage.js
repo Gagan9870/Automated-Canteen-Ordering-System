540af8ce5065582ce76500d4f1d7724ed4e37141
Gagan Saraswat <gagan.saraswat_cs18@gla.ac.in>
2020-11-08 22:14:55 +0530

    Home page js

--- JavaScript Files/homepage.js
+++ JavaScript Files/homepage.js
@@ -2,11 +2,16 @@ var box = document.querySelector(".box");
 var signin = document.getElementById("sign-in");
 var signup = document.getElementById("sign-up");
 var banner = document.querySelector(".banner");
+var signUp = document.getElementById("signUp");
 // var signInForm = document.querySelector(".newSignin");
 
 
 signin.onclick = signInFormCreate;
 signup.onclick =signUpFormCreate;
+signUp.onclick= alertMsg;
+function alertMsg(){
+    alert("You have Signed up Successfully... \nClick on the Sign in button to Contiinue... ");
+}
 
 function signInFormCreate(){
     signin.style.backgroundColor ="black";
@@ -28,6 +33,7 @@ function signInFormCreate(){
     newUserBox.classList.add("newUserBox");
     newUserBox.setAttribute("type","username");
     newUserBox.setAttribute("placeholder","Enter UserName");
+    newUserBox.required=true;
     newSignin.appendChild(newUserBox);
 
     var newPassword = document.createElement("label");
@@ -39,11 +45,13 @@ function signInFormCreate(){
     newPasswordBox.classList.add("newPasswordBox");
     newPasswordBox.setAttribute("type","password");
     newPasswordBox.setAttribute("placeholder","Enter Password");
+    newPasswordBox.required=true;
     newSignin.appendChild(newPasswordBox);
 
     var chkbox = document.createElement("input");
     chkbox.classList.add("chkbox");
     chkbox.setAttribute("type","checkbox");
+    chkbox.required=true;
     newSignin.appendChild(chkbox);
 
     var para = document.createElement("p");
@@ -54,6 +62,7 @@ function signInFormCreate(){
     var newButton = document.createElement("a");
     newButton.classList.add("newButton");
     newButton.innerHTML ="SIGN-IN";
+    newButton.setAttribute("href","../Automated-Canteen-Ordering-System/Blog/AfterSignIn.html")
     newSignin.appendChild(newButton);
 
     banner.appendChild(newSignin);
